63d5422e7d9010c19c0399ef3041fcd0460787ab
carlosgarciarayo201 <carlos@carlos-VirtualBox>
2020-06-08 11:23:49 +0200

    quito pass

--- src/main/java/com/cgr/appcine/security/DatabaseWebSecurity.java
+++ src/main/java/com/cgr/appcine/security/DatabaseWebSecurity.java
@@ -1,29 +1,29 @@
-package com.cgr.appcine.security;
-
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-@EnableWebSecurity
-public class DatabaseWebSecurity extends WebSecurityConfigurerAdapter {
-	
-	@Autowired
-	private DataSource dataSource;
-
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		
-		auth.jdbcAuthentication().dataSource(dataSource)
-		.usersByUsernameQuery("select username, password, status,  from user where username=?")
-		.authoritiesByUsernameQuery("select u.username, p.profiles from pk_user_profile up " +
-			"inner join user u on u.id = up.idUser " +
-			"inner join profiles p on p.id = up.idProfile " +
-			"where u.username = ?");
-		
-	}
-}
+//package com.cgr.appcine.security;
+//
+//import javax.sql.DataSource;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class DatabaseWebSecurity extends WebSecurityConfigurerAdapter {
+//	
+//	@Autowired
+//	private DataSource dataSource;
+//
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		
+//		auth.jdbcAuthentication().dataSource(dataSource)
+//		.usersByUsernameQuery("select username, password, status,  from user where username=?")
+//		.authoritiesByUsernameQuery("select u.username, p.profiles from pk_user_profile up " +
+//			"inner join user u on u.id = up.idUser " +
+//			"inner join profiles p on p.id = up.idProfile " +
+//			"where u.username = ?");
+//		
+//	}
+//}
